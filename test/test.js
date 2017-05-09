97ab6d228ab2fe68b607d571b384bd2a57d95300
Sarath <sarath.hacks@gmail.com>
2017-05-09 18:57:48 +0530

    Update

--- test/test.js
+++ test/test.js
@@ -2,18 +2,13 @@
 /**Test.js */
 var assert = require('assert');
 
-var add = require('../src/index')
+var calculator = require('../src/index')
 
-describe("Array",function(){
+describe("Main Module",function(){
 
-    it("Should return -1 when the value is not present",function(){
+    it("Shout have the calculator property",function(){
 
-        assert.equal(-1,[1,2,3].indexOf(4))
-    })
-
-    it("It should return sum",function(){
-
-        assert.equal(5,add(2,3));
-    })
+        assert.equal('object',typeof(calculator.calculator));
+    });
 
 })
